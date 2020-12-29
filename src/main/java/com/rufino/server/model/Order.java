e2dce064eb7028e08392732b13ad00ff25697807
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-12-29 14:06:20 -0300

    add order model

---
+++ src/main/java/com/rufino/server/model/Order.java
@@ -0,0 +1,45 @@
+package com.rufino.server.model;
+
+import java.util.UUID;
+
+public class Order {
+    
+    private UUID orderId;
+    private String orderPaymentMethod;
+    private String orderDescription;
+    private String orderCreatedAt;
+
+    public UUID getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(UUID orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderPaymentMethod() {
+        return orderPaymentMethod;
+    }
+
+    public void setOrderPaymentMethod(String orderPaymentMethod) {
+        this.orderPaymentMethod = orderPaymentMethod;
+    }
+
+    public String getOrderDescription() {
+        return orderDescription;
+    }
+
+    public void setOrderDescription(String orderDescription) {
+        this.orderDescription = orderDescription;
+    }
+
+    public String getOrderCreatedAt() {
+        return orderCreatedAt;
+    }
+
+    public void setOrderCreatedAt(String orderCreatedAt) {
+        this.orderCreatedAt = orderCreatedAt;
+    }
+
+    
+}
