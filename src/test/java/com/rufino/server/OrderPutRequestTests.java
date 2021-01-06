e7cd644fd95dcffbe44e2a204f9fabfa756b5198
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-01-06 15:00:33 -0300

    Updating valid fields - ignoring incorrect ones

--- src/test/java/com/rufino/server/OrderPutRequestTests.java
+++ src/test/java/com/rufino/server/OrderPutRequestTests.java
@@ -85,6 +85,33 @@ public class OrderPutRequestTests {
                 .andReturn();
     }
 
+    @Test
+    void itShouldUpdatePartiallyOrder() throws Exception {
+        JSONObject my_obj = new JSONObject();
+
+        Order order1 = new Order();
+        setOrder(order1, "cba3ff2e-3087-49bd-bc9b-285e809e7b32");
+        saveAndAssert(order1);
+
+        Order order2 = new Order();
+        setOrder(order2, "846e1a32-f831-4bee-a6bc-673b5f901d7b");
+        saveAndAssert(order2, 1, 2);
+
+        my_obj.put("customerId", "846e1a32");
+        my_obj.put("orderTotalValue", 5.99);
+        my_obj.put("orderPaymentMethod", "cash");
+        my_obj.put("orderNumber", 123123);
+
+        mockMvc.perform(put("/api/v1/order/" + order1.getOrderId()).contentType(MediaType.APPLICATION_JSON)
+                .content(my_obj.toString()))
+                .andExpect(
+                        MockMvcResultMatchers.jsonPath("$.customerId", Is.is("cba3ff2e-3087-49bd-bc9b-285e809e7b32")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.orderTotalValue", Is.is(5.99)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.orderPaymentMethod", Is.is("cash")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.orderNumber", Is.is(123123))).andExpect(status().isOk())
+                .andReturn();
+    }
+
     private void saveAndAssert(Order order) {
         long countBeforeInsert = jdbcTemplate.queryForObject("select count(*) from orders", Long.class);
         assertEquals(0, countBeforeInsert);
