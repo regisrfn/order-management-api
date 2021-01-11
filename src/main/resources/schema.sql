1f88d36eac2c2b98eee13d00b21f6a6a556268d8
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-01-11 11:17:27 -0300

    only handling order table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,3 @@
--- ************************************** customers
-
-CREATE TABLE IF NOT EXISTS customers
-(
- customer_id         uuid NOT NULL,
- customer_name       varchar(50) NOT NULL,
- customer_last_name  varchar(100) NOT NULL,
- customer_phone      varchar(50) NOT NULL,
- customer_email      varchar(50) NOT NULL,
- customer_created_at timestamp with time zone NOT NULL,
- CONSTRAINT PK_customers PRIMARY KEY ( customer_id ),
- CONSTRAINT ind_email UNIQUE ( customer_email )
-);
-
 -- ************************************** orders
 
 CREATE TABLE IF NOT EXISTS orders
